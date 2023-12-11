f619bccc5754520416eaca1241ea1266e546983c
Aditya Naithani <aditya0602@gmail.com>
2023-12-11 18:43:02 -0500

    add dna validation middleware, finish up dna analysis functions

--- dna/dna.js
+++ dna/dna.js
@@ -1,10 +1,27 @@
+// validate dna middleware
+const validDna = /^[ATCG]+$/;
+
+export function validateDNA(req, res, next) {
+  const { dna } = req.body;
+
+  if (!validDna.test(dna)) {
+    return res.status(400).json({
+      error:
+        "Invalid DNA sequence. Provide a sequence containing only A, T, C, G.",
+    });
+  }
+
+  next();
+}
+
 // translate to RNA
-export async function translateToRNA(dna) {
+export function translateToRNA(dna) {
+  console.log(dna);
   return dna.replace(/T/g, "U");
 }
 
 // reverse complement
-export async function reverseComplement(dna) {
+export function reverseComplement(dna) {
   const complement = { A: "T", C: "G", G: "C", T: "A" };
   return dna
     .split("")
@@ -14,22 +31,27 @@ export async function reverseComplement(dna) {
 }
 
 // gc content calculation
-export async function gcContent(dna) {
-  const gc = dna.match(/[GC]/g).length;
-  const at = dna.match(/[AT]/g).length;
+export function gcContent(dna) {
+  const gc = (dna.match(/[GC]/g) || []).length;
+  const at = (dna.match(/[AT]/g) || []).length;
+
+  if (gc + at === 0) {
+    return 0;
+  }
+
   return (gc / (gc + at)) * 100;
 }
 
 // nucleotide counts
-export async function nucleotideCounts(dna) {
+export function nucleotideCounts(dna) {
   const counts = { A: 0, C: 0, G: 0, T: 0 };
   dna.split("").forEach((nucleotide) => counts[nucleotide]++);
   return counts;
 }
 
 // nucleotide frequency
-export async function nucleotideFrequency(dna) {
-  const counts = await nucleotideCounts(dna);
+export function nucleotideFrequency(dna) {
+  const counts = nucleotideCounts(dna);
   const total = dna.length;
   return Object.keys(counts).reduce((acc, nucleotide) => {
     acc[nucleotide] = counts[nucleotide] / total;
--- dna/index.js
+++ dna/index.js
@@ -4,12 +4,13 @@ import helmet from "helmet";
 import winston from "winston";
 import cors from "cors";
 import {
+  validateDNA,
   translateToRNA,
   reverseComplement,
   gcContent,
   nucleotideCounts,
   nucleotideFrequency,
-} from "./dna";
+} from "./dna.js";
 
 const log = winston.createLogger({
   level: "info",
@@ -30,12 +31,44 @@ app.use(cors());
 app.use(express.json());
 
 // translate route
+app.post("/translate", validateDNA, async (req, res) => {
+  const { dna } = req.body;
+  const rna = translateToRNA(dna);
+  // add to db
+  res.status(200).json({ rna });
+});
 
 // reverse complement route
+app.post("/reverse-complement", validateDNA, async (req, res) => {
+  const { dna } = req.body;
+  const reverse = reverseComplement(dna);
+  // add to db
+  res.status(200).json({ reverse });
+});
 
 // gc content route
+app.post("/gc-content", validateDNA, async (req, res) => {
+  const { dna } = req.body;
+  const gc = gcContent(dna);
+  // add to db
+  res.status(200).json({ gc });
+});
 
 // nucleotide counts route
+app.post("/nucleotide-counts", validateDNA, async (req, res) => {
+  const { dna } = req.body;
+  const counts = nucleotideCounts(dna);
+  // add to db
+  res.status(200).json({ counts });
+});
+
+// nucleotide frequency route
+app.post("/nucleotide-frequency", validateDNA, async (req, res) => {
+  const { dna } = req.body;
+  const frequency = nucleotideFrequency(dna);
+  // add to db
+  res.status(200).json({ frequency });
+});
 
 app.listen(PORT, () => {
   log.info(`Listening on port ${PORT}`);
